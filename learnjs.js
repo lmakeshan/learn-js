8da4387abd6bc2db9aeea27851bf06f7ad1f99ea
lmakeshan <2511947893@qq.com>
2020-07-24 11:43:17 +0800

    第一章

--- learnjs.js
+++ learnjs.js
@@ -1,13 +1,45 @@
 // typeof 返回值的类型
+//一元运算符
 console.log(typeof 4.5)
 // => numbe/
 console.log(typeof "x")
 //=>string
 console.log(- (10 - 2))
 // -8
+//布尔值
 console.log(3 > 2)
 // true
 console.log(3 < 2)
 // false
 console.log("Aardvark" < "Zoroaster")
 // true
+console.log("Apple" == "orange")
+// false
+console.log(NaN == NaN)
+//  false
+//逻辑运算符
+console.log(true && false)
+// false
+console.log(true && true)
+// true
+console.log(false || true)
+// true
+console.log(false || false)
+// false
+console.log(true ? 1 : 2);
+// 1
+console.log(false ? 1 : 2);
+// 2
+//自动类型切换
+console.log(8 * null)
+// 0
+console.log("5" - 1)
+// 4
+console.log("5" + 1)
+// 51
+console.log("five" * 2)
+// NaN
+console.log(false == 0 )
+// true
+
+
